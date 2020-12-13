16cf0f2517101c2f82b3f43163ea23810b14e737
hannah.mazri <hannah.mazri@terang.asia>
2020-12-13 15:01:29 +0800

    Display initial list of movies

--- src/app/store/reducers/movie.reducer.ts
+++ src/app/store/reducers/movie.reducer.ts
@@ -10,8 +10,62 @@ const initialState: Movie[] = [
   },
   {
     id: 1,
-    title: 'End Game',
+    title: 'Avengers: Endgame',
     rating: 4.8,
+    numberOfVotes: 1
+  },
+  {
+    id: 2,
+    title: 'Star Wars: The Force Awakens',
+    rating: 4.4,
+    numberOfVotes: 1
+  },
+  {
+    id: 3,
+    title: 'Skyfall',
+    rating: 4.6,
+    numberOfVotes: 1
+  },
+  {
+    id: 4,
+    title: 'Interstellar',
+    rating: 4.6,
+    numberOfVotes: 1
+  },
+  {
+    id: 5,
+    title: 'Knives Out',
+    rating: 4.8,
+    numberOfVotes: 1
+  },
+  {
+    id: 6,
+    title: 'Knives Out',
+    rating: 4.8,
+    numberOfVotes: 1
+  },
+  {
+    id: 7,
+    title: 'Thor: Ragnarok',
+    rating: 4.8,
+    numberOfVotes: 1
+  },
+  {
+    id: 8,
+    title: 'Gattaca',
+    rating: 4.7,
+    numberOfVotes: 1
+  },
+  {
+    id: 8,
+    title: 'Gattaca',
+    rating: 4.7,
+    numberOfVotes: 1
+  },
+  {
+    id: 9,
+    title: 'Men in Black: International',
+    rating: 4.4,
     numberOfVotes: 1
   }
 ];
